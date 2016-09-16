32c545c64913dc6f47ac8ee3cfd942b748aee9a6
agomezomg <anagomez98@unitec.edu>
2016-09-16 13:46:39 -0600

    :,)

--- Guerrero.cpp
+++ Guerrero.cpp
@@ -1,7 +1,7 @@
 {\rtf1\ansi\ansicpg1252\cocoartf1404\cocoasubrtf340
 {\fonttbl\f0\fswiss\fcharset0 Helvetica;\f1\fnil\fcharset0 AppleSymbols;}
 {\colortbl;\red255\green255\blue255;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\paperw11900\paperh16840\margl1440\margr1440\vieww27120\viewh15360\viewkind0
 \pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
 
 \f0\fs24 \cf0 #include <sstream>\
@@ -26,6 +26,7 @@ Guerrero::Guerrero(string name, int HP, double offence, double defence, int forc
 		cout << \'93incorrect values, setting offence to 10%\'94;\
 		this -> offence = 0.1;\
 	\}\
+\
 	if(force > 50 || force < 0) \{\
 		cout << \'93value not permitted, setting force to max lvl\'85 \\n\'94;\
 		this -> force = 50;\
@@ -61,4 +62,20 @@ void Guerrero::setHP(int HP) \{\
 \
 double Guerrero::getOffence() \{\
 	return this -> offence;\
-\}}
+\}\
+\
+void Guerrero::setOffence(double offence) \{\
+	if(offence > 1) \{\
+		this -> offence = offence/100;\
+	\} else if (offence > 0 && offence < 1) \{\
+		this -> offence = offence;\
+	\} else \{\
+		cout << \'93No changes\'94;\
+	\}\
+\}\
+\
+double Guerrero::getDefence() \{\
+	return this -> defence;\
+\}\
+\
+}
